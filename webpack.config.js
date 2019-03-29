5bd00e349dd51980d853f9ba62e686cc1343fe65
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-03-29 16:30:50 -0400

    babel working

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           "loader": "babel-loader",
           "options": {
             "presets": [
-              "env"
+              "@babel/preset-env"
             ]
           }
         }
